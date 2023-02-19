59928f2cd77ad95609cacc172f5ac68071f223c1
Alfeen Sadiq <115562186+Alfe3n@users.noreply.github.com>
2023-02-19 21:24:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Content from './components/content';
 import Navbar from './components/navbar';
 function App() {
-
+  
+  
   return (
     <div className="App">
       <Navbar />
